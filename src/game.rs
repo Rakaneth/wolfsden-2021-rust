c2f0a057091c397005be7707c0c12580f2ffa63c
Quincy West <rakaneth@gmail.com>
2021-01-11 21:39:06 -0500

    basic entities

--- src/game.rs
+++ src/game.rs
@@ -1,43 +1,61 @@
 use coord_2d::{Coord, Size};
 use direction::CardinalDirection;
-
-pub struct GameState {
-    screen_size: Size,
-    player_coord: Coord,
-}
-
-impl GameState {
-    pub fn new(screen_size: Size) -> Self {
-        Self {
-            player_coord: screen_size.to_coord().unwrap() / 2,
-            screen_size,
-        }
-    }
-
-    pub fn maybe_move_player(&mut self, direction: CardinalDirection) {
-        let new_player_coord = self.player_coord + direction.coord();
-        if new_player_coord.is_valid(self.screen_size) {
-            self.player_coord = new_player_coord
-        }
-    }
-
-    pub fn player_coord(&self) -> Coord {
-        self.player_coord
-    }
-}
-
-/*
-#[derive(Clone, Copy, Debug)]
-pub enum Tile {
-    Player,
-}
+use entity_table::{Entity, EntityAllocator};
 
 entity_table::declare_entity_module! {
-    components: {
+    components {
         coord: Coord,
         tile: Tile,
     }
 }
 
 use components::Components;
-*/
+
+pub struct GameState {
+    screen_size: Size,
+    components: Components,
+    player_entity: Entity,
+}
+
+impl GameState {
+    fn spawn_player(&mut self, coord: Coord) {
+        self.components.coord.insert(self.player_entity, coord);
+        self.components.tile.insert(self.player_entity, Tile::Player);
+    }
+
+    fn populate(&mut self, player_coord: Coord) {
+        self.spawn_player(player_coord);
+    }
+    pub fn new(screen_size: Size) -> Self {
+        let mut entity_allocator = EntityAllocator::default();
+        let components = Components::default();
+        let player_entity = entity_allocator.alloc();
+        let mut game_state = Self {
+            screen_size,
+            components,
+            player_entity
+        };
+        game_state.populate(screen_size.to_coord().unwrap() / 2);
+        game_state
+    }
+
+    pub fn maybe_move_player(&mut self, direction: CardinalDirection) {
+        let player_coord = self.components.coord.get_mut(self.player_entity).expect("player has no coord component");
+        let new_player_coord = *player_coord + direction.coord();
+        if new_player_coord.is_valid(self.screen_size) {
+            *player_coord = new_player_coord;
+        }
+    }
+
+    pub fn player_coord(&self) -> Coord {
+        *self.components.coord.get(self.player_entity).expect("Player has no coord component")
+    }
+}
+
+
+#[derive(Clone, Copy, Debug)]
+pub enum Tile {
+    Player,
+}
+
+
